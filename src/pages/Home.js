5c44a07599ae5c0967ecd7c36ee1e616765b797a
OscarRuscalleda <oscarruscalleda@gmail.com>
2023-01-31 23:24:28 +0100

    [ADD] Creació de Home
    Creació de la pàgina principal del projecte.

---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from "react"
+import { Container } from "@mui/material"
+import { Grid }from "@mui/material"
+import NavBar from "../components/NavBar"
+import PokeCard from "../components/PokeCard"
+import PokeStats from "../components/PokeStats"
+import axios from "axios"
+import { FooterContainer } from "../React Footer/Footer"
+
+export const Home = () => {
+    const [pokemon, setPokemon] = useState([]);
+    useEffect(() => {
+        getPokemon();
+    }, []);
+    const getPokemon = () => {
+        var endpoints = [];
+        for (var i = 1; i < 151; i++) {
+            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+        }
+        axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemon(res));
+    };
+    
+    return (
+    <div>
+      <NavBar/>
+      <Container maxWidth="xg">
+        <Grid container spacing={3}>
+            {pokemon.map((pokemon, key) => (
+                <Grid item xs={2} key={key}>
+                    <PokeCard  id={pokemon.data.id} name={pokemon.data.name} image={pokemon.data.sprites.front_default}/>
+                </Grid>
+            ))}
+        </Grid> 
+      </Container>
+      <PokeStats/>
+      <FooterContainer/>
+    </div>
+  )
+}
