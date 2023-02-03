a046c105e9d9a3cfaa26cf4ce9e082d03935569d
OscarRuscalleda <oscarruscalleda@gmail.com>
2023-02-03 17:55:31 +0100

    [BUG]
    Solució d'erros

--- src/components/PokeCard2/index.js
+++ src/components/PokeCard2/index.js
@@ -1,10 +1,13 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Box from '@mui/material/Box';
 
 export default function PokeCard({id, name, image, pokeInfo, setShowPokemonDetail, setPokemonDetail }) {
   
   return (
-    <Card sx={{ maxWidth: 345, backgroundColor: "black"}}>
     <Card  
       sx={{ maxWidth: 345, backgroundImage:"url(/imatges/bg.png)",backgroundSize:"cover"}}
       onClick={() => { 
@@ -18,7 +21,6 @@ export default function PokeCard({id, name, image, pokeInfo, setShowPokemonDetai
           {id.toString().padStart(3, '0')}
         </Typography>
         </Box>
-        <CardMedia component="img" height={200} image={image}></CardMedia>
         <CardMedia component="img" height={240} image={image}></CardMedia>
         <Box xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%', backgroundColor: "#3c3c3c"}}>
         <Typography gutterBottom variant="h5"  paddingTop="5px" fontWeight= "bold" color= "#fff" component="div">
@@ -27,7 +29,6 @@ export default function PokeCard({id, name, image, pokeInfo, setShowPokemonDetai
         </Box>
       </CardActionArea>
     </Card>
-    </Card>   
 
   );
 }
