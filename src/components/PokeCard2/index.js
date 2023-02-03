88b4b62ac5942f32280e950037324c64bdb488df
OscarRuscalleda <oscarruscalleda@gmail.com>
2023-02-03 17:42:15 +0100

    [UPDATE] PokeCard2 Millorat
    S'han solucionat error i fet millores

--- src/components/PokeCard2/index.js
+++ src/components/PokeCard2/index.js
@@ -1,13 +1,17 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Box from '@mui/material/Box';
 
-export default function PokeCard({id, name, image}) {
+export default function PokeCard({id, name, image, pokeInfo, setShowPokemonDetail, setPokemonDetail }) {
+  
   return (
     <Card sx={{ maxWidth: 345, backgroundColor: "black"}}>
+    <Card  
+      sx={{ maxWidth: 345, backgroundImage:"url(/imatges/bg.png)",backgroundSize:"cover"}}
+      onClick={() => { 
+        setShowPokemonDetail(true) 
+        setPokemonDetail(pokeInfo)
+      }}
+    >
       <CardActionArea>
         <Box xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '40px', backgroundColor: "#3c3c3c"}}>
           <Typography gutterBottom variant="h5" paddingTop="5px" fontWeight= "bold" color= "#fff" component="div">
@@ -15,6 +19,7 @@ export default function PokeCard({id, name, image}) {
         </Typography>
         </Box>
         <CardMedia component="img" height={200} image={image}></CardMedia>
+        <CardMedia component="img" height={240} image={image}></CardMedia>
         <Box xs={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', width: '100%', backgroundColor: "#3c3c3c"}}>
         <Typography gutterBottom variant="h5"  paddingTop="5px" fontWeight= "bold" color= "#fff" component="div">
           {name.replace(/^./,(str) => str.toUpperCase())}
@@ -22,5 +27,7 @@ export default function PokeCard({id, name, image}) {
         </Box>
       </CardActionArea>
     </Card>
+    </Card>   
+
   );
 }
